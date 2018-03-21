b30b06ca9ad4ba55302870dab9bd683e449e8f09
Shaheer Mir <shaheermir0@gmail.com>
2018-03-21 10:19:34 -0400

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB.')
+  }
+  console.log('Connected to MongoDB server.')
+
+  /*
+  db.collection('Todos').insertOne(
+    {
+      text: 'Something to do.',
+      completed: false
+    },
+    (err, result) => {
+      if (err) {
+        return console.log('Unable to insert todo.', err)
+      }
+      console.log(JSON.stringify(result.ops, null, 2))
+    }
+  )*/
+
+  /*
+  db.collection('Users').insertOne(
+    {
+      name: 'Shaheer',
+      age: 24,
+      location: 'Toronto'
+    },
+    (err, result) => {
+      if (err) {
+        return console.log('Unable to insert todo.', err)
+      }
+      console.log(result.ops[0]._id.getTimestamp())
+    }
+  )*/
+
+  db.close()
+})
