5d6402973e212a9283533cd8651e696c2b86f740
Shaheer Mir <shaheermir0@gmail.com>
2018-03-21 13:27:56 -0400

    Setup test suite. Adding unit tests for routes.

--- server/server.js
+++ server/server.js
@@ -16,13 +16,15 @@ app.post('/todos', (req, res) => {
   todo
     .save()
     .then(doc => {
-      console.log('Saved todo', doc)
+      //console.log('Saved todo', doc)
       res.json(doc)
     })
     .catch(e => {
-      console.log('Failed to save todo', e)
+      //console.log('Failed to save todo', e)
       res.status(400).send(e)
     })
 })
 
 app.listen(3000, () => console.log('Started on port 3000.'))
+
+module.exports = { app }
