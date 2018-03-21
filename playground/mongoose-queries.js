f68b3bdaa842bd23ddb48731f868f914ecc7f983
Shaheer Mir <shaheermir0@gmail.com>
2018-03-21 15:48:14 -0400

    Exploring mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,26 @@
+const { ObjectID } = require('mongodb')
+const { mongoose } = require('../server/db/mongoose')
+const { Todo } = require('../server/models/Todo')
+
+const id = '5ab2b0bff8589d200d01dba3'
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid.')
+}
+
+Todo.find({
+  _id: id
+}).then(todos => console.log('Todos', todos))
+
+Todo.findOne({
+  _id: id
+}).then(todo => console.log('Todo', todo))
+
+Todo.findById(id)
+  .then(todo => {
+    if (!todo) {
+      return console.log('Id not found.')
+    }
+    console.log('Todo By Id', todo)
+  })
+  .catch(err => console.log(err))
