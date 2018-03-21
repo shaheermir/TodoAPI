c3e97644791ae4c561a64c126288b161a74b651f
Shaheer Mir <shaheermir0@gmail.com>
2018-03-21 12:06:50 -0400

    Added POST /todos route.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,28 @@
-const { mongoose } = require('./db/mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
 
+const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
 const { User } = require('./models/User')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  })
+
+  todo
+    .save()
+    .then(doc => {
+      console.log('Saved todo', doc)
+      res.json(doc)
+    })
+    .catch(e => {
+      console.log('Failed to save todo', e)
+      res.status(400).send(e)
+    })
+})
+
+app.listen(3000, () => console.log('Started on port 3000.'))
