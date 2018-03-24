b2957509771ec269ea0015d553d96697ff7e3049
Shaheer Mir <shaheermir0@gmail.com>
2018-03-24 18:40:57 -0400

    Added tests for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,44 @@ describe('GET /todos/:id', () => {
       .end(done)
   })
 })
+
+describe('DELTE /todos/:id', () => {
+  it('should remoe a todo', done => {
+    const id = todos[0]._id.toHexString()
+
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(id)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.findById(id)
+          .then(todo => {
+            expect(todo).toBeFalsy()
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+
+  it('should return 404 if todo not found', done => {
+    const id = new ObjectID().toHexString()
+
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return 404 if ObjectID is invalid', done => {
+    request(app)
+      .delete(`/todos/abc123`)
+      .expect(404)
+      .end(done)
+  })
+})
