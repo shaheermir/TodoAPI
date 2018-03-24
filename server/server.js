69ad76cbd9c1b7d9c310ffe9d679bab8b1ca6612
Shaheer Mir <shaheermir0@gmail.com>
2018-03-24 19:00:44 -0400

    Added PATCH /todos/:id route.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
+const _ = require('lodash')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
@@ -58,6 +59,32 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(404).send()
       }
       res.json({ todo })
+    })
+    .catch(err => res.status(400).send())
+})
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id
+  const body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+
+      res.send({ todo })
     })
     .catch(err => res.status(400).send())
 })
