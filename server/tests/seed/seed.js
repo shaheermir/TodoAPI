136a17ec812807ab0570d61bc68d9fef42da16de
Shaheer Mir <shaheermir0@gmail.com>
2018-03-27 14:52:16 -0400

    Added tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,66 @@
+const { ObjectID } = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+const { Todo } = require('../../models/Todo')
+const { User } = require('../../models/User')
+
+const userOneId = new ObjectID()
+const userTwoId = new ObjectID()
+
+const users = [
+  {
+    _id: userOneId,
+    email: 'shaheer1@example.com',
+    password: 'userOnePass',
+    tokens: [
+      {
+        access: 'auth',
+        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
+      }
+    ]
+  },
+  {
+    _id: userTwoId,
+    email: 'shaheer2@example.com',
+    password: 'userTwoPass'
+  }
+]
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: 'First test todo'
+  },
+  {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+  }
+]
+
+const populateTodos = done => {
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos)
+    })
+    .then(() => done())
+}
+
+const populateUsers = done => {
+  User.remove({})
+    .then(() => {
+      const userOne = new User(users[0]).save()
+      const userTwo = new User(users[1]).save()
+
+      return Promise.all([userOne, userTwo])
+    })
+    .then(() => done())
+}
+
+module.exports = {
+  users,
+  todos,
+  populateTodos,
+  populateUsers
+}
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,27 +4,11 @@ const { ObjectID } = require('mongodb')
 
 const { app } = require('../server')
 const { Todo } = require('../models/Todo')
+const { User } = require('../models/User')
+const { todos, users, populateTodos, populateUsers } = require('./seed/seed')
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First test todo'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-  }
-]
-
-beforeEach(done => {
-  Todo.remove({})
-    .then(() => {
-      return Todo.insertMany(todos)
-    })
-    .then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
   it('should create a new Todo', done => {
@@ -183,6 +167,86 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.completed).toBe(false)
         expect(res.body.todo.completedAt).toBeFalsy()
       })
+      .end(done)
+  })
+})
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', done => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(res => {
+        expect(res.body._id).toBe(users[0]._id.toHexString())
+        expect(res.body.email).toBe(users[0].email)
+      })
+      .end(done)
+  })
+
+  it('should return a 401 if not authenticated', done => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect(res => {
+        expect(res.body).toEqual({
+          name: 'JsonWebTokenError',
+          message: 'jwt must be provided'
+        })
+      })
+      .end(done)
+  })
+})
+
+describe('POST /users', () => {
+  it('should create a user', done => {
+    const email = 'example@example.com'
+    const password = 'abc123'
+
+    request(app)
+      .post('/users')
+      .send({ email, password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toBeTruthy()
+        expect(res.body._id).toBeTruthy()
+        expect(res.body.email).toBe(email)
+      })
+      .end(err => {
+        if (err) {
+          return done()
+        }
+
+        User.findOne({ email }).then(user => {
+          expect(user).toBeTruthy()
+          expect(user.password).not.toBe(password)
+          done()
+        })
+      })
+  })
+
+  it('should return validation errors for invalid requests', done => {
+    const email = 'blah-blah-blah'
+    const password = 'abc'
+
+    request(app)
+      .post('/users')
+      .send({ email, password })
+      .expect(400)
+      .expect(res => {
+        expect(res.body.errors).toBeTruthy()
+      })
+      .end(done)
+  })
+
+  it('should not create user if email is in use', done => {
+    request(app)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: 'testPassword'
+      })
+      .expect(400)
       .end(done)
   })
 })
